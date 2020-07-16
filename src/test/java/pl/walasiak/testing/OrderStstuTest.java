5f395b614dc3288f5794eca4b059221249162c08
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:32:43 +0200

    initial commit

--- src/test/java/pl/walasiak/testing/OrderStstuTest.java
+++ src/test/java/pl/walasiak/testing/OrderStstuTest.java
@@ -1,4 +1,17 @@
 package pl.walasiak.testing;
 
-public class OrderStstuTest {
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+
+class OrderStstuTest {
+
+    @ParameterizedTest
+    @EnumSource(OrderStatus.class)
+    void allOrderStatusShouldBeLessThan15Characters(OrderStatus orderStatus){
+        assertThat(orderStatus.toString().length(), lessThan(15));
+    }
 }
