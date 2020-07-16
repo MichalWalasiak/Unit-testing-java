d33cdfa379f233295043e7ef6fa380b65a899664
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:30:43 +0200

    initial commit

--- src/main/java/pl/walasiak/testing/Adress.java
+++ src/main/java/pl/walasiak/testing/Adress.java
@@ -3,9 +3,9 @@ package pl.walasiak.testing;
 public class Adress {
 
     private String street;
-    private int number;
+    private String number;
 
-    public Adress(String street, int number) {
+    public Adress(String street, String number) {
         this.street = street;
         this.number = number;
     }
