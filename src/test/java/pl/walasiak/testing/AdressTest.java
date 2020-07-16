c2026f8cafbb89d7d401309a1c264a080010dedb
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:31:04 +0200

    initial commit

--- src/test/java/pl/walasiak/testing/AdressTest.java
+++ src/test/java/pl/walasiak/testing/AdressTest.java
@@ -1,4 +1,29 @@
 package pl.walasiak.testing;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class AdressTest {
+
+    @ParameterizedTest
+    @CsvSource({"Puławska, 45", "Armii Krajowej, 57/10", "'Tytusa, Romka, Atomka', 7/4"})
+    void givenAdressShouldHaveNumbersAndHaveProperNumber(String name, String number){
+        assertThat(name, notNullValue());
+        assertThat(name, containsString("a"));
+        assertThat(number, notNullValue());
+        assertThat(number.length(), lessThan(8));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/adresses.csv")
+    void adressesFromFileSourceShouldHaveNameAndHaveProperNumber(String name, String number){
+        assertThat(name, notNullValue());
+        assertThat(name, containsString("a"));
+        assertThat(number, notNullValue());
+        assertThat(number.length(), lessThan(8));
+    }
 }
