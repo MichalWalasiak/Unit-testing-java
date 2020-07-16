9e4c4ee4b68fca90aa7c8a8f626907f5ead492a2
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:31:15 +0200

    initial commit

--- src/main/java/pl/walasiak/testing/BeforeAfterExtension.java
+++ src/main/java/pl/walasiak/testing/BeforeAfterExtension.java
@@ -1,4 +1,16 @@
 package pl.walasiak.testing;
 
-public class BeforeAfterExtension {
+import org.junit.jupiter.api.extension.*;
+
+public class BeforeAfterExtension implements BeforeEachCallback, AfterEachCallback {
+
+    @Override
+    public void beforeEach(final ExtensionContext extensionContext) throws Exception {
+        System.out.println("inside before each extension");
+    }
+
+    @Override
+    public void afterEach(final ExtensionContext extensionContext) throws Exception {
+        System.out.println("inside after each extension");
+    }
 }
