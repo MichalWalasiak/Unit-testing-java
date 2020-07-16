e66928b1c44e03ad64eacf8af2e7609c2abd0f2e
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:32:51 +0200

    initial commit

--- src/test/java/pl/walasiak/testing/OrderTest.java
+++ src/test/java/pl/walasiak/testing/OrderTest.java
@@ -3,16 +3,17 @@ package pl.walasiak.testing;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@ExtendWith(BeforeAfterExtension.class)
 class OrderTest {
 
     private Order order;
