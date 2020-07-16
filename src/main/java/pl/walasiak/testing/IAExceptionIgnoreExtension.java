bebda77ddf2f51eab47d1051fb9c9d7cbde1685e
Michał Walasiak <m.walasiak@gmail.com>
2020-07-16 10:31:45 +0200

    initial commit

--- src/main/java/pl/walasiak/testing/IAExceptionIgnoreExtension.java
+++ src/main/java/pl/walasiak/testing/IAExceptionIgnoreExtension.java
@@ -1,4 +1,22 @@
 package pl.walasiak.testing;
 
-public class IAExceptionIgnoreExtension {
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class IAExceptionIgnoreExtension implements TestExecutionExceptionHandler {
+
+    private static Logger logger = Logger.getLogger(IAExceptionIgnoreExtension.class.getName());
+
+    @Override
+    public void handleTestExecutionException(final ExtensionContext extensionContext, final Throwable throwable) throws Throwable {
+
+        if (throwable instanceof IllegalArgumentException){
+            logger.info("just ignore IllegalArgumentException");
+        }else{
+            throw throwable;
+        }
+    }
 }
