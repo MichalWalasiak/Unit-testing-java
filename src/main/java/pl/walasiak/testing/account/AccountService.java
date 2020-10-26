06ab5dd2963b886fbd59976144949ef66813b536
Michał Walasiak <m.walasiak@gmail.com>
2020-10-26 22:07:39 +0100

    create constructor

--- src/main/java/pl/walasiak/testing/account/AccountService.java
+++ src/main/java/pl/walasiak/testing/account/AccountService.java
@@ -2,5 +2,9 @@ package pl.walasiak.testing.account;
 
 public class AccountService {
 
+    AccountRepository accountRepository;
 
+    public AccountService(final AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 }
