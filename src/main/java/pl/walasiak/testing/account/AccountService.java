16dba09a1604b8272f7ece6cfabf254a9a1116f9
Michał Walasiak <m.walasiak@gmail.com>
2020-10-26 22:10:25 +0100

    add method

--- src/main/java/pl/walasiak/testing/account/AccountService.java
+++ src/main/java/pl/walasiak/testing/account/AccountService.java
@@ -1,10 +1,19 @@
 package pl.walasiak.testing.account;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AccountService {
 
     AccountRepository accountRepository;
 
     public AccountService(final AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    List<Account> getAllActiveAccounts() {
+        return accountRepository.getAllAccounts().stream()
+                .filter(Account::isActive)
+                .collect(Collectors.toList());
     }
 }
