2d21187720c5453e4035a055763e04dabe571081
Michał Walasiak <m.walasiak@gmail.com>
2020-10-28 22:35:37 +0100

    change class name

--- src/test/java/pl/walasiak/testing/account/AccountServiceStubTest.java
+++ src/test/java/pl/walasiak/testing/account/AccountServiceStubTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
 
-class AccountServiceTest {
+class AccountServiceStubTest {
 
     @Test
     void getAllActiveAccounts() {
