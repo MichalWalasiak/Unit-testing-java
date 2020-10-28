1ba4653a894f450d61f63d446712b9110659b065
Michał Walasiak <m.walasiak@gmail.com>
2020-10-28 22:42:14 +0100

    work in progress

--- src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
+++ src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
@@ -2,20 +2,20 @@ package pl.walasiak.testing.account;
 
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+
+import static org.mockito.Mockito.mock;
 
 public class AccountServiceTest {
 
     // Given
-    AccountRepository accountRepositoryStub = new AccountRepositoryStub();
+    AccountRepository accountRepositoryStub = mock(AccountRepository.class);
     AccountService accountService = new AccountService(accountRepositoryStub);
 
     // When
     List<Account> list = accountService.getAllActiveAccounts();
 
     // Then
-    assertThat(list, hasSize(2));
+    assertThat()
 
 
 }
