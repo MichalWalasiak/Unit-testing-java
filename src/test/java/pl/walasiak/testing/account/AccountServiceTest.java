73aa05263ecfe32170bf7fbf42d41cddcca16a41
Michał Walasiak <m.walasiak@gmail.com>
2020-10-28 22:39:13 +0100

    work in progress

--- src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
+++ src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
@@ -1,6 +1,21 @@
 package pl.walasiak.testing.account;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
 public class AccountServiceTest {
+
+    // Given
+    AccountRepository accountRepositoryStub = new AccountRepositoryStub();
+    AccountService accountService = new AccountService(accountRepositoryStub);
+
+    // When
+    List<Account> list = accountService.getAllActiveAccounts();
+
+    // Then
+    assertThat(list, hasSize(2));
 
 
 }
