0064e6002d35670ffa2fa1486c550a3efbbb918c
Michał Walasiak <m.walasiak@gmail.com>
2020-10-29 21:32:49 +0100

    work with unit tests

--- src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
+++ src/test/java/pl/walasiak/testing/account/AccountServiceTest.java
@@ -1,21 +1,30 @@
 package pl.walasiak.testing.account;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.mock;
 
 public class AccountServiceTest {
 
-    // Given
-    AccountRepository accountRepositoryStub = mock(AccountRepository.class);
-    AccountService accountService = new AccountService(accountRepositoryStub);
+    @Test
+    void shouldreturnAllActiveAccounts(){
 
-    // When
-    List<Account> list = accountService.getAllActiveAccounts();
+        // Given
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
 
-    // Then
-    assertThat()
+        // When
+        List<Account> list = accountService.getAllActiveAccounts();
+
+        // Then
+        assertThat(list, hasSize(2));
+
+    }
 
 
 }
