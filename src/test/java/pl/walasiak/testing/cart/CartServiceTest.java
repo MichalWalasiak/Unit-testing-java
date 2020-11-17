5c68524659cd0799e6922b5b437efa09ae06b3c0
Michał Walasiak <m.walasiak@gmail.com>
2020-11-17 22:23:49 +0100

    add test method and assertions

--- src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
+++ src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
@@ -1,6 +1,7 @@
 package pl.walasiak.testing.cart;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import pl.walasiak.testing.order.Order;
 import pl.walasiak.testing.order.OrderStatus;
@@ -9,8 +10,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 public class CartServiceTest {
 
@@ -31,7 +32,38 @@ public class CartServiceTest {
 
         //then
         verify(cartHandler).sendToPrepeare(cart);
+        verify(cartHandler, times(1)).sendToPrepeare(cart);
+        verify(cartHandler, atLeastOnce()).sendToPrepeare(cart);
+
+
+
+        InOrder inOrder = inOrder(cartHandler);
+
+        inOrder.verify(cartHandler).canHandleCart(cart);
+        inOrder.verify(cartHandler).sendToPrepeare(cart);
         assertThat(resultCart.getOrders(), hasSize(1));
         assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARED));
+    }
+
+    @Test
+    void processCartShouldNotSendOrderToPrepeare() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+        given(cartHandler.canHandleCart(cart)).willReturn(false);
+
+        //when
+        Cart resultCart = cartService.processCart(cart);
+
+        //then
+        verify(cartHandler, never()).sendToPrepeare(cart);
+        then(cartHandler).should(never()).sendToPrepeare(cart);
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.REJECTED));
     }
 }
