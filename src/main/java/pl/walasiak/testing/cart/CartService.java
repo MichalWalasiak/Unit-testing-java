6e45296be4e83e72db3a5ef69ef4f56b30d18b25
Michał Walasiak <m.walasiak@gmail.com>
2020-11-17 21:37:16 +0100

    add method

--- src/main/java/pl/walasiak/testing/cart/CartService.java
+++ src/main/java/pl/walasiak/testing/cart/CartService.java
@@ -1,10 +1,27 @@
 package pl.walasiak.testing.cart;
 
+import pl.walasiak.testing.order.OrderStatus;
+
 public class CartService {
 
     private CartHandler cartHandler;
 
     public CartService(final CartHandler cartHandler) {
         this.cartHandler = cartHandler;
+    }
+
+    Cart processCart(Cart cart) {
+        if (cartHandler.canHandleCart(cart)){
+            cartHandler.sendToPrepeare(cart);
+            cart.getOrders().forEach(order -> {
+                order.changeOrderStatus(OrderStatus.PREPARED);
+            });
+            return cart;
+        }else{
+            cart.getOrders().forEach(order -> {
+                order.changeOrderStatus(OrderStatus.REJECTED);
+            });
+            return cart;
+        }
     }
 }
