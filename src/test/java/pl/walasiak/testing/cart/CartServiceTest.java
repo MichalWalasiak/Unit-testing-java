c5281ebe19721c292875e9656425928a0ce57ef8
Michał Walasiak <m.walasiak@gmail.com>
2020-11-17 21:47:44 +0100

    add test method

--- src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
+++ src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
@@ -1,4 +1,11 @@
 package pl.walasiak.testing.cart;
 
+import org.junit.jupiter.api.Test;
+
 public class CartServiceTest {
+
+    @Test
+    void processCartShouldSendOrderToPrepeare() {
+
+    }
 }
