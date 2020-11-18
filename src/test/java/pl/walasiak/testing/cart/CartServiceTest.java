eebacb0caeff41659461d66c7612b23a5dee3f68
Michał Walasiak <m.walasiak@gmail.com>
2020-11-18 22:39:39 +0100

    add test method

--- src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
+++ src/test/java/pl/walasiak/testing/cart/CartServiceTest.java
@@ -108,4 +108,27 @@ public class CartServiceTest {
         assertThat(cartHandler.canHandleCart(cart), equalTo(true));
 
     }
+
+    @Test
+    void processCartShouldSendOrderToPrepeareWithLambdas() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+        given(cartHandler.canHandleCart(cart)).willReturn(true);
+
+        //when
+        Cart resultCart = cartService.processCart(cart);
+
+        //then
+        then(cartHandler).should().sendToPrepeare(cart);
+
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARED));
+    }
+
 }
